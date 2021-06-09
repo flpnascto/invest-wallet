9f8294b118bf380dcff0521cecbd31fffbbc223f
Felipe Nascimento <flpnascto@gmail.com>
2021-06-09 10:16:34 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./src/routes');
+require('dotenv').config();
+
+const { PORT } = process.env;
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Online');
+});
+
+app.listen(PORT, () => console.log('Online'));
